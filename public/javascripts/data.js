a971c1dad400bf5c66e1f9eaa3c66ceca3b133b3
Joao Stein <joaosteing@gmail.com>
2013-12-16 15:34:59 -0800

    Add some generic data

---
+++ public/javascripts/data.js
@@ -0,0 +1,22 @@
+var data = [
+  {
+    quality: 60,
+    duration: 6,
+    date: new Date(),
+  },
+  {
+    quality: 80,
+    duration: 8,
+    date: new Date(),
+  },
+  {
+    quality: 94,
+    duration: 10,
+    date: new Date(),
+  },
+  {
+    quality: 56,
+    duration: 4,
+    date: new Date(),
+  }
+];
