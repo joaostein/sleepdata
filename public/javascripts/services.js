6ada5b251fc31ef95242fe6726bc0eceea784705
Joao Stein <joaosteing@gmail.com>
2013-12-16 16:13:28 -0800

    Add a basic service to create svg element

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,7 +1,16 @@
 angular.module('sleepData')
   .factory('D3Service', function () {
     var service = {
+      createSvg: function (width, height, element) {
+        var width = width;
+        var height = height;
+        var svg = d3.select(element).append('svg').attr({
+          'width': width,
+          'height': height
+        });
 
+        return svg;
+      }
     };
 
     return service;
--- public/javascripts/sleepData.js
+++ public/javascripts/sleepData.js
@@ -7,7 +7,13 @@ angular.module('sleepData', [])
       restrict: 'A',
       scope: {},
       link: function (scope, element, attrs) {
-
+        var svg = D3Service.createSvg(960, 600, element[0]);
+        var elements = svg.append('g').selectAll('texts').data(data);
+        elements.enter().append('circle').attr({
+          r: function (data, index) {
+            console.log(data);
+          }
+        });
       }
     }
   })
