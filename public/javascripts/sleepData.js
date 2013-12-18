0facfb26601691588072a75b3a7a7463fa18aeb2
Joao Stein <joaosteing@gmail.com>
2013-12-17 16:19:45 -0800

    Add different line graphs for each month

--- public/javascripts/sleepData.js
+++ public/javascripts/sleepData.js
@@ -1,5 +1,19 @@
 d3.csv("javascripts/data.csv", function(error, row) {
-  var data = [];
+  var data = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+    11: [],
+    12: []
+  };
+
   for (var i = 0; i < row.length; i++) {
     if (row[i]['Sleep quality'].split('%')[0] < 10) {
       continue;
@@ -9,24 +23,21 @@ d3.csv("javascripts/data.csv", function(error, row) {
     night.x = parseFloat(row[i]['Time in bed'].split(':').join('.')) + Math.random();
     night.y = row[i]['Sleep quality'].split('%')[0];
     night.minutes = row[i]['Time in bed'].split(':')[1];
-    data.push(night);
+    night.month = (new Date(row[i]['Start'])).getMonth() + 1;
+    data[night.month].push(night);
   }
 
-
-  data.forEach(function (d) {
-    console.log( d.x );
-    console.log( Math.floor(Math.round(parseFloat(d.x)*100)/100) );
-  });
-
-  var data = data.sort(function compare(a, b) {
-    var x1 = Math.round(parseFloat(a.x)*100)/100;
-    var x2 = Math.round(parseFloat(b.x)*100)/100;
-    if (x1 < x2)
-      return -1;
-    if (x1 > x2)
-      return 1;
-    return 0;
-  });
+  for (month in data) {
+    data[month] = data[month].sort(function (a, b) {
+      var x1 = Math.round(parseFloat(a.x)*100)/100;
+      var x2 = Math.round(parseFloat(b.x)*100)/100;
+      if (x1 < x2)
+        return -1;
+      if (x1 > x2)
+        return 1;
+      return 0;
+    });
+  }
 
   createLineGraph(data);
   createHistoricalBarChart(data);
